53e0260887e75f76ae27fb2b9775843e6aee499e
Rohit Padile <rohitpadile24@gmail.com>
2024-07-17 21:53:12 +0530

    minor changes

--- src/Services/apiServiceAdmin.js
+++ src/Services/apiServiceAdmin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/admin/'; // Ensure the URL has the trailing slash
-
+// const BASE_URL = 'http://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/admin/'; // Ensure the URL has the trailing slash
+const BASE_URL = 'http://localhost:8080/api/v1/admin/'; // Ensure the URL has the trailing slash
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
