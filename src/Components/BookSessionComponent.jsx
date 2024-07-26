8dd3177a40f0bfde7384cc93e0ad6314bf56c477
Rohit Padile <rohitpadile24@gmail.com>
2024-07-26 18:06:20 +0530

    styling version - 2 : student profile and pop up for book session component

--- src/Components/BookSessionComponent.jsx
+++ src/Components/BookSessionComponent.jsx
@@ -1,35 +1,32 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../App.css';
-
+import '../css/BookSessionComponentCss.css'; // Use the new CSS file
 
 const BookSessionComponent = ({ student, onClose }) => {
-  // const { student } = location.state || {};
   const navigate = useNavigate();
 
   const handleBookZoomSession = () => {
-    // navigate(`/bookZoomSessionForm/${student.studentName}`);
     navigate('/bookZoomSessionForm', { state: { student } });
   };
 
   return (
-    <div className="container mt-5">
-      <div className="card book-session-card mx-auto" style={{ maxWidth: '800px' }}>
-        <div className="card-header book-session-header">
-          <h4>Book Session</h4>
+    <div className="book-session-popup">
+      <div className="card book-session-card mx-auto">
+        <div className="card-header book-session-header d-flex justify-content-between align-items-center">
+          <h4 className="text-center">Book Session</h4>
           <button className="btn-close" onClick={onClose}>X</button> {/* Close button */}
         </div>
-        <div className="card-body book-session-body">
-          <h5>{student.studentName}</h5>
+        <div className="card-body book-session-body text-center">
+          <h5 className='text-center'>{student.studentName}</h5>
           <div className="list-group mt-4">
             <button
-              className="list-group-item list-group-item-action btn btn-primary mb-2"
+              className="list-group-item list-group-item-action btn btn-primary mb-2 text-center"
               onClick={handleBookZoomSession}
             >
               Book 1:1 Zoom Session
             </button>
-            <button className="list-group-item list-group-item-action btn btn-secondary">
-              Book 1:1 Offline Session
+            <button className="list-group-item list-group-item-action btn btn-secondary text-center">
+              Book 1:1 Offline Session (Currently Unavailable)
             </button>
           </div>
         </div>
