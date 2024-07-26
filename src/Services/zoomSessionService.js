ca4cfeee5a2d5d3c263c1ca55dea7c38163c87a7
Rohit Padile <rohitpadile24@gmail.com>
2024-07-26 22:25:09 +0530

    Removed web.config.js added for building it without console.log in production. It gave errors

--- src/Services/zoomSessionService.js
+++ src/Services/zoomSessionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const BASE_URL = 'http://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/admin/';
-const BASE_URL = 'http://localhost:8080/api/v1/admin/';
+const BASE_URL = 'http://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/admin/';
+// const BASE_URL = 'http://localhost:8080/api/v1/admin/';
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
