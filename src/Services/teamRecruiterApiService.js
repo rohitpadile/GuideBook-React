2b646cc0d7f64af444c3d1428f0641e1e48e28f9
Rohit Padile <rohitpadile24@gmail.com>
2024-08-01 00:45:07 +0530

    Website is successfully deployed on AWS! Cheers!

---
+++ src/Services/teamRecruiterApiService.js
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+// // const BASE_URL = 'https://guidebookx.com/api/v1/teamRecruiter/';
+// const BASE_URL = 'https://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
+// // const BASE_URL = 'http://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
+// // const BASE_URL = 'http://localhost:8080/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
+// const api = axios.create({
+//   baseURL: BASE_URL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
