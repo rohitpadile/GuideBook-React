3751be1ae511b84c9ff376f76afda7d9ddfab3b1
Rohit Padile <rohitpadile24@gmail.com>
2024-08-04 18:37:39 +0530

    Change api address for deployment. Lets go!

--- src/Services/teamRecruiterApiService.js
+++ src/Services/teamRecruiterApiService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 // const BASE_URL = 'https://guidebookx.com/api/v1/teamRecruiter/';
 // const BASE_URL = 'https://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
 // const BASE_URL = 'http://guidebookX-alb-1586257955.ap-south-1.elb.amazonaws.com/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
-const BASE_URL = 'http://localhost:8080/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
+// const BASE_URL = 'http://localhost:8080/api/v1/teamRecruiter/'; // Ensure the URL has the trailing slash
 // const BASE_URL = "https://www.guidebookx.com/api/v1/teamRecruiter";
-// const BASE_URL = "https://api.guidebookx.com/api/v1/teamRecruiter";
+const BASE_URL = "https://api.guidebookx.com/api/v1/teamRecruiter";
 // https://chatgpt.com/c/5d78c49f-e2be-44f4-8334-b033d2e06b60
 // const BASE_URL = process.env.REACT_APP_BASE_URL; // for production purpose
 
